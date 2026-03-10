5dcafbc2542e505b0608e47e420a665f69de2fcb
Foh Shawn Young <shawn.foh@gmail.com>
2026-03-10 16:46:27 +0800

    Update sqlalchemy create_engine parameter.

--- app/core/database.py
+++ app/core/database.py
@@ -10,7 +10,12 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not found in environment variables")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    pool_recycle=300,
+    pool_size=5,
+    max_overflow=10)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
